1853d7a0fa9b47247d8ced8226688c08a952c343
Guilherme Hirata <guilherme.hirata@ibm.com>
2026-02-24 10:34:54 -0300

    teste CI/CD

--- src/test/java/com/guilherme/automation/base/BaseTest.java
+++ src/test/java/com/guilherme/automation/base/BaseTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class BaseTest {
 
@@ -13,6 +14,14 @@ public class BaseTest {
     @BeforeEach
     void setUp() {
         WebDriverManager.firefoxdriver().setup();
+
+        // Teste CI/CD
+        FirefoxOptions options = new FirefoxOptions();
+        boolean headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+        if (headless) {
+            options.addArguments("-headless");
+        }
+
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
     }
